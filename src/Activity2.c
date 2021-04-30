1b8896962d300bfddbe0c50b04b5645608f55205
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 17:53:57 +0530

    Update Activity2.c

--- src/Activity2.c
+++ src/Activity2.c
@@ -11,23 +11,23 @@
 #include<avr/io.h>
 #include "Activity2.h"
 
-uint16_t ReadADC(uint8_t ch)
-{
-    //Select ADC Channel ch must be 0-7
-    ADMUX&=0xf8;
-    ch=ch&0b00000111;
-    ADMUX|=ch;
-    //Start single conversion
-    ADCSRA|=(1<<ADSC);
-    //Wait for conversion to complete
-    while(!(ADCSRA & (1<<ADIF)));
-    //Clear ADIF by writing one to it
-    ADCSRA|=(1<<ADIF);
-    return(ADC);
-}
-void InitADC()
+
+void ADC_Init()
 {
     ADMUX=(1<<REFS0);
     ADCSRA=(1<<ADEN)|(7<<ADPS0);
     
 }
+
+uint16_t ReadADC(uint8_t channel)
+{
+    //ADC Channel selected must be in the range 0-7
+    ADMUX&=0xf8;
+    ch=ch&0b00000111;
+    ADMUX|=ch;
+    ADCSRA|=(1<<ADSC);
+    while(!(ADCSRA & (1<<ADIF)));
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+}
+
