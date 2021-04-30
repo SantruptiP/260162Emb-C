661606cb620cae18dd205d477c99cbe8ef917bc9
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 10:38:02 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,1 +1,22 @@
-
+/**
+ * @file Activity3.h
+ * @author 260162
+ * @brief Display the output of the temperature sensor
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __ACTIVITY3_H__
+#define __ACTIVITY3_H__
+#include<avr/io.h>
+/**
+ * @brief Function definition
+ * 
+ * @param temp_value 
+ * @return char
+ */
+char OutPWM(uint16_t temp_value);
+void InitPWM(void);
+#endif
