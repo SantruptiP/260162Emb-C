69a9a5e014c45a92ccc9022e960ac37307565aae
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 18:28:38 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -1,7 +1,7 @@
 /**
  * @file Activity3.c
  * @author 260162
- * @brief Configure the PWM wave according to temperature
+ * @brief Calculating PWM for the specific temperature 
  * @version 0.1
  * @date 2021-04-27
  * 
@@ -12,46 +12,46 @@
 #include<util/delay.h>
 #include<avr/io.h>
 /**
- * @brief initializing timer and counter register
+ * @brief Counter and timer variable Initialized
  * 
  */
-void InitPWM(void)
+void PWM_Init(void)
 {
-    /*Configuring the registers and ports*/
+    /*Registers and ports are Configured*/
     TCCR1A|=(1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
     TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10);
     DDRB|=(1<<PB1);
 
 }
 /**
- * @brief Output with respect to analog input
+ * @brief analog input gives the respective output
  * 
- * @param ADC_val 
+ * @param val_ADC 
  */
-char OutPWM (uint16_t ADC_val)
+char OutPWM (uint16_t val_ADC)
 {
     
     char temperature;
 
-    if((ADC_val>=0) && (ADC_val<=209)){
+    if((val_ADC>=0) && (val_ADC<=200)){
 
-        OCR1A = 205; //20% duty cycle
+        OCR1A = 205; //20% duty cycle 20%
         temperature = 20;
         _delay_ms(20);
     }
-    else if((ADC_val>=210) && (ADC_val<=509)){
+    else if((val_ADC>=201 && (val_ADC<=500)){
 
         OCR1A = 410; //40% duty cycle
         temperature = 25;
        _delay_ms(20);
     }
-    else if((ADC_val>=510) && (ADC_val<=709)){
+    else if((val_ADC>=501) && (val_ADC<=700)){
 
         OCR1A = 717;//70% duty cycle
         temperature = 29;
         _delay_ms(20);
     }
-    else if((ADC_val>=710) && (ADC_val<=1024)){
+    else if((val_ADC>=701) && (val_ADC<=1024)){
 
         OCR1A = 973; //95% duty cycle
         temperature = 33;
