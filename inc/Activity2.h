c7a444be3e3cb7780cd94345ee151aca0bc071d1
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 10:13:21 +0530

    Create Activity2.h

---
+++ inc/Activity2.h
@@ -0,0 +1,22 @@
+/**
+ * @file Activity2.h
+ * @author 260162 (you@domain.com)
+ * @brief ADC function definition
+ * @version 0.1
+ * @date 2021-04-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __READADC_H__
+#define __READADC_H__
+/**
+ * @brief Reading the analog input and returning a digital 8bit value
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t ReadADC(uint8_t ch);
+void InitADC();
+#endif
