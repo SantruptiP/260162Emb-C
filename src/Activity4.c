a11d41cbfc213d32cc7a629601651624081353e3
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 18:37:06 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -1,24 +1,24 @@
 /**
  * @file Activity4.c
  * @author 260162
- * @brief Function definition of UART serial communication
+ * @brief UART serial communication function definition
  * @version 0.1
  * @date 2021-04-28
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#include<avr/io.h>
-#include"Activity4.h"
+#include <avr/io.h>
+#include "Activity4.h"
 /**
- * @brief Port initialization
+ * @brief Initialization of Port
  * 
- * @param ubrr_value 
+ * @param value_ubrr 
  */
-void InitUART(uint16_t ubrr_value){
+void UART_Init(uint16_t value_ubrr){
 
-    UBRR0L = ubrr_value;
-    UBRR0H = (ubrr_value>>8)&(0x00ff);
+    UBRR0L = value_ubrr;
+    UBRR0H = (value_ubrr>>8)&(0x00ff);
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ01);
 
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
@@ -26,15 +26,15 @@ void InitUART(uint16_t ubrr_value){
 }
 
 /**
- * @brief write data on UART ports
+ * @brief Data written on UART ports
  * 
- * @param data 
+ * @param spec 
  */
-void UARTwrite(char data)
+void UARTwrite(char spec)
 {
     while(!(UCSR0A & (1<<UDRE0)))
     {
 
     }
-    UDR0 = data;
+    UDR0 = spec;
 }
