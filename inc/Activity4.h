85a4478c30abe94e5faed6e7c54b15d93fe4f16d
SantruptiP <82207640+SantruptiP@users.noreply.github.com>
2021-04-30 10:57:10 +0530

    Create Activity4.h

---
+++ inc/Activity4.h
@@ -0,0 +1,33 @@
+/**
+ * @file Activity.h
+ * @author 260162
+ * @brief UART serial communication
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ACTIVITY4_H__
+#define __ACTIVITY4_H__
+#include<avr/io.h>
+/**
+ * @brief Inittialize UART serial communication ports
+ * 
+ * @param ubrr 
+ */
+void InitUART(uint16_t ubrr);
+/**
+ * @brief Read characters from UART port
+ * @return char
+ */
+char UARTread();
+/**
+ * @brief Write characters to UART port
+ * 
+ * @param data 
+ */
+void UARTwrite(char data);
+
+#endif
